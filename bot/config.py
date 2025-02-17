77476b05030aea5d69bb5c1569c84d2e0cc1ce9e
ak4422920 <ak4422920@gmail.com>
2025-02-17 14:10:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,13 +13,13 @@ class Telegram:
     SESSION_STRING = getenv("SESSION_STRING", "")
     BASE_URL = getenv("BASE_URL", "https://github.com/ak4422920/Surf-TG").rstrip('/')
     DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://tg:tg@cluster0.fekvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "-1002475275261,-1002386873051,-1002333030990,-1002295131184,-1002435507517").split(",") if channel.strip()]
+    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "-1002386873051,-1002333030990,-1002295131184,-1002435507517").split(",") if channel.strip()]
     THEME = getenv("THEME", "quartz").lower()
     USERNAME = getenv("USERNAME", "admin")
     PASSWORD = getenv("PASSWORD", "admin")
     ADMIN_USERNAME = getenv("ADMIN_USERNAME", "cine")
     ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "cine")
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '10'))
+    WORKERS = int(getenv('WORKERS', '20'))
     MULTI_CLIENT = getenv('MULTI_CLIENT', 'False')
     HIDE_CHANNEL = getenv('HIDE_CHANNEL', 'False')
